a74e6ef2b46b7d8dbb6c40d539fd08503e039fe8
Radmila-code <chaplygina1983@gmail.com>
2025-01-13 22:36:53 -0700

    adjusting navbar settings and mobile adaptation

--- src/App.js
+++ src/App.js
@@ -44,7 +44,10 @@ function App() {
           <Link className="navbar-brand" to="/">
             <img src={`${process.env.PUBLIC_URL}/images/image16.png`} alt="Sweet Story Short" className="navbar-logo"/>
           </Link>
-          <div className="collapse navbar-collapse">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/">Home</Link>
