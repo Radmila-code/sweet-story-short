28f49b5895036899d3f3f74110ab93ca01e1ce9d
Radmila-code <chaplygina1983@gmail.com>
2025-01-15 21:51:31 -0700

    added Navbar.js and Navbar.css

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar'; // Подключаем новый компонент Navbar
 import Home from './Home';
 import Products from './Products';
 import Contact from './Contact';
 import Gallery from './Gallery';
 import Cart from './Cart';
 import './styles.css';
-import './Gallery.css';
-import './Home.css';
-import './Contact.css';
-import './Products.css';
-import './Cart.css';
 
 function App() {
-  const cartItems = useSelector(state => state.cartItems);
-  const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
-
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link className="navbar-brand" to="/">
-            <img src={`${process.env.PUBLIC_URL}/images/image16.png`} alt="Sweet Story Short" className="navbar-logo"/>
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/products">Products</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/gallery">Gallery</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <Link className="nav-link cart-link" to="/cart">
-              Cart ({cartItemCount})
-            </Link>
-          </div>
-        </nav>
+        <Navbar /> {/* Используем компонент Navbar */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
---
+++ src/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'; // Подключаем стили для Navbar
+
+function Navbar() {
+  const cartItems = useSelector((state) => state.cartItems);
+  const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        <img src={`${process.env.PUBLIC_URL}/images/image16.png`} alt="Sweet Story Short" className="navbar-logo" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/products">
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/gallery">
+              Gallery
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <Link className="nav-link cart-link" to="/cart">
+          Cart ({cartItemCount})
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
